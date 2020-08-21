d99eead6d03177226d2402065bf4b8249c4c868f
AlexandrrrIdushy <alexandriyxx@gmail.com>
2020-08-21 20:13:22 +0400

    переделал функцию Ждать_приема_таких_байт()

--- VS/TestHarvester/TestHarvester/HexComm.cs
+++ VS/TestHarvester/TestHarvester/HexComm.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace TestHarvester
 {
-    //пользовательские основные команды применяемые для тестирования обмена
+
+    public enum ResWaitBytes
+    {
+        //название как продолжение фразы результат работы функции _______
+        Непонятен_неизвестен,
+        Не_уложилась_в_заданное_время,
+        Неверный_формат_последовательнсти_байт,
+        Байтов_слишком_мало,
+        Успешный
+    }
+
+    public enum SimplResult
+    {
+        //простой результат
+        OK,
+        Wrong
+    }
+
+    public class ResWaitBytesFoo
+    {
+        public ResWaitBytes Detailed;
+        public SimplResult Simple;
+    }
+
+
+        //пользовательские основные команды применяемые для тестирования обмена
     public class SimpleComm
     {
         COM _com;
@@ -15,20 +40,16 @@ namespace TestHarvester
             _com = com;
         }
 
-        public enum ResWaitBytes
-        {
-            Undef,
-            Timeout,
-            PatternIsWrong,
-            ByteIsSmall,
-            Succes
-        }
 
-        public ResWaitBytes Ждать_приема_таких_байт(string needBytes, float sec)
+
+
+
+        public ResWaitBytesFoo Ждать_приема_таких_байт(string needBytes, float sec)
         {
             List<byte> bytesOfPort = new List<byte>();
             List<byte> bytesOfPattern = new List<byte>();
-            ResWaitBytes result = ResWaitBytes.Undef;
+            ResWaitBytes detailedResult = ResWaitBytes.Непонятен_неизвестен;
+            SimplResult simplResult = SimplResult.Wrong;
             string []  bytesOfPatternArr = needBytes.Split(' ');
             try
             {
@@ -39,24 +60,31 @@ namespace TestHarvester
             }
             catch (Exception)
             {
-                return ResWaitBytes.PatternIsWrong;
+                detailedResult = ResWaitBytes.Неверный_формат_последовательнсти_байт;
                 throw;
             }
 
-
-            COM.ResRcvNBytes resultRcv = _com.ReceiveNByte(ref bytesOfPort, (short)(bytesOfPattern.Count), sec);
-            if (resultRcv == COM.ResRcvNBytes.TimeOut)
-                return ResWaitBytes.Timeout;
-            else if(resultRcv == COM.ResRcvNBytes.NBytesIsSmall)
-                return ResWaitBytes.ByteIsSmall;
-            else if(resultRcv == COM.ResRcvNBytes.Succes)
+            if (detailedResult != ResWaitBytes.Неверный_формат_последовательнсти_байт)
             {
-                if(bytesOfPort.SequenceEqual(bytesOfPattern))
-                    return ResWaitBytes.Succes;
-            }
-                
+                COM.ResRcvNBytes resultRcv = _com.ReceiveNByte(ref bytesOfPort, (short)(bytesOfPattern.Count), sec);
+                if (resultRcv == COM.ResRcvNBytes.TimeOut)
+                    detailedResult = ResWaitBytes.Не_уложилась_в_заданное_время;
+                else if (resultRcv == COM.ResRcvNBytes.NBytesIsSmall)
+                    detailedResult = ResWaitBytes.Байтов_слишком_мало;
+                else if (resultRcv == COM.ResRcvNBytes.Succes)
+                {
+                    if (bytesOfPort.SequenceEqual(bytesOfPattern))
+                    {
+                        detailedResult = ResWaitBytes.Успешный;
+                        simplResult = SimplResult.OK;
+                    }
+                }
 
-            return ResWaitBytes.Undef;
+            }
+            ResWaitBytesFoo res = new ResWaitBytesFoo();
+            res.Simple = simplResult;
+            res.Detailed = detailedResult;
+            return res;
         }
 
         public void Отправить_такие_байты(string bytes)
